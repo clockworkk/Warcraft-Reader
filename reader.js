d7959074062e574fdca596d55714e88271622c5c
pmiddleton <pmiddleton@arbor.net>
2016-08-27 11:09:54 -0400

    Made changes so that the chrome extension works, currently only supports current settings

--- reader.js
+++ reader.js
@@ -1,7 +1,11 @@
-
 //Change the background Color of the posts to white.
-[].forEach.call(document.getElementsByClassName('topic-post'), function(v, i, a) {
+[].forEach.call(document.getElementsByClassName('TopicPost-bodyContent'), function(v, i, a) {
 	a[i].style.background = 'transparent';
 	a[i].style.backgroundColor = '#ffffff';
 	a[i].style.color = '#000000';
-} );
+});
+
+// Change the background Color of the blockquotes
+[].forEach.call(document.getElementsByTagName('blockquote'), function(v, i, a) {
+  a[i].style.backgroundColor = '#191919';
+});
